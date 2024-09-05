d5965a1ad0d7bad11323ae32c07171dc7611ab1b
João Vitor <criticossemdiploma@gmail.com>
2024-09-05 12:16:04 -0300

    atualizado

--- anime/js/script.js
+++ anime/js/script.js
@@ -1,5 +1,8 @@
 // Usar o método find para buscar o num_capitulo com base no nome do manga (case insensitive)
 const nomeAnimeBuscado = localStorage.getItem('titulo');
+let container_episodios = document.getElementById('episodios');
+let container_video = document.getElementById('box_video');
+let FecharVideo = document.getElementById('fechar_Video');
 
 
 const animeEncontrado = lista_animes.find(anime => anime.nome.toLowerCase() === nomeAnimeBuscado.toLocaleLowerCase());
@@ -17,7 +20,26 @@ if (animeEncontrado) {
     document.getElementById('sinopse').innerHTML = AnimeSinopse;
     document.getElementById('dublagem').innerText = AnimeDublagem;
     document.getElementById('lancado').innerText = AnimeLacamento;
+    document.getElementById('titulo_pagina').innerText = AnimeNome;
 
-    document.querySelector('#imagen img').src = `.${AnimeImg}`
-    document.getElementById('Imagen_banner').src = `.${AnimeImg}`
+    document.querySelector('#imagen img').src = `.${AnimeImg}`;
+    document.getElementById('Imagen_banner').src = `.${AnimeImg}`;
+
+    for(i = 1; i <= 2; i++) {
+        container_episodios.innerHTML += `
+           <p class="episodio">
+            <span><i class="fas fa-play"></i></span>
+            ${AnimeNome} - episodio 0${i} - Postado 26 de fevereiro
+          </p>
+        `;
+    }
 }
+
+FecharVideo.onclick = function() {
+  container_video.remove()
+}
+
+
+
+
+
---
+++ assets/js/animes.js
@@ -0,0 +1,94 @@
+let lista_animes = [
+    {
+        "nome": 'bleach thousand-year blood war',
+        "lancamento": 2024,
+        "img": './imgs/Bleach.webp',
+        "episodios": 23,
+        "sinopse" : 'Foi tudo apenas uma coincidência, ou foi inevitável? Ichigo Kurosaki ganhou os poderes de um Soul Reaper através de um encontro casual. Como um Ceifador de Almas Substituto, Ichigo foi pego no tumulto da Soul Society, um lugar onde as almas falecidas se reúnem. Mas com a ajuda de seus amigos, Ichigo superou todos os desafios para se tornar ainda mais forte. Quando novos Soul Reapers e um novo inimigo aparecem em sua cidade natal, Karakura, Ichigo volta ao campo de batalha com sua Zanpakuto para ajudar os necessitados. Enquanto isso, a Soul Society está observando um aumento repentino no número de Hollows sendo destruídos no Mundo dos Vivos. Eles também recebem relatórios separados de moradores do distrito de Rukon desaparecidos. Finalmente, o Seireitei, lar dos Soul Reapers, é atacado por um grupo que se autodenomina Wandenreich. Liderados por Yhwach, o pai de todos os Quincys, os Wandenreich declaram guerra contra os Soul Reapers com a seguinte mensagem: "Daqui a cinco dias, a Soul Society será aniquilada pelos Wandenreich." A história e a verdade mantidas escondidas pelos Soul Reapers por mil longos anos são finalmente trazidas à luz. Todas as coisas devem chegar ao fim quando a batalha final de Ichigo Kurosaki começar! (Fonte: site oficial, traduzido)',
+        "generos" : 'A16 Ação Aventura Fantasia',
+        "dublagem" : 'Legendado/Dublado',
+    },
+    {
+        "nome": 'jujutsu no kaisen',
+        "lancamento": 2024,
+        "img": './imgs/jujutsu no kaisen.jpg',
+        "episodios": 23,
+        "sinopse" : 'Jujutsu Kaisen Yuji é um gênio do atletismo, mas não tem interesse algum em ficar correndo em círculos. Ele é feliz como membro no Clube de Estudo de Fenômenos Psíquicos. Apesar de estar no clube apenas por diversão, tudo fica sério quando um espírito de verdade aparece na escola! A vida está prestes a se tornar muito interessante na Escola Sugisawa',
+        "generos" : 'Ação Demônios Dublado Fantasia Legendado Shounen Sobrenatural',
+        "dublagem" : 'Legendado/Dublado',
+    },
+    {
+        "nome": 'Boruto',
+        "lancamento": 2024,
+        "img": './imgs/Boruto.jpg',
+        "episodios": 223,
+        "sinopse" : 'Naruto era um jovem shinobi com um talento incorrigível para travessuras. Ele conseguiu seu sonho de se tornar o maior ninja da aldeia e seu rosto fica no topo do monumento Hokage. Mas esta não é a sua história … Uma nova geração de ninjas estão prontos para subir ao palco, liderados pelo próprio filho de Naruto, Boruto!',
+        "generos" : 'Ação  Artes Marciais  Aventura Comédia Fantasia Legendado Shounen Super Poderes',
+        "dublagem" : 'Legendado/Dublado',
+    },
+    {
+        "nome": 'Chiyu Mahou no Machigatta',
+        "lancamento": 2023,
+        "img": './imgs/Chiyu Mahou no Machigatta.jpg',
+        "episodios": 223,
+        "sinopse" : 'Uma caminhada comum de volta para casa após a escola se transforma em uma jornada épica para Usato! Após ser subitamente transportado para outro mundo juntamente de dois colegas de classe, Usato descobre que foi convocado lá por acidente. No entanto, as coisas mudam de água pro vinho quando ele descobre que tem uma habilidade única para a magia de cura! Agora, ele treina além das limitações humanas, utilizando suas habilidades de autocura para ganhar uma força absurda e uma resistência incomparável.',
+        "generos" : 'Ação Fantasia Legendado',
+        "dublagem" : 'Legendado',
+    },
+    {
+        "nome": 'Tokyo Ghoul',
+        "lancamento": 2023,
+        "img": './imgs/Tokyo Ghoul.webp',
+        "episodios": 26,
+        "sinopse" : 'Tokyo Ghoul Todos os Episodios Online, Assistir Tokyo Ghoul Anime Completo, Assistir Tokyo Ghoul Online.Estranhos assassinatos começam a acontecer em Tokyo. Devido a evidência líquida nos casos, a polícia concluiu que os ataques são resultados de uma criatura que se alimenta de outros seres, um “ghoul” do tipo “eater”. Dois amigos de faculdade, Kaneki e Hide, criam a teoria de que os ghouls estão imitando os humanos, por isso nunca são vistos ou capturados. Eles nem imaginam que essa teoria pode ser verdade e a coisa sairá do controle, pois Kaneki acaba se envolvendo, sem saber, com uma dessas criaturas, que ao atacar o garoto acaba morrendo acidentalmente, mas o deixa seriamente ferido e a ponto de morrer. A única salvação? Um transplante envolvendo a própria criatura, mas que poderá trazer consequências terríveis para Kaneki. O que acontecerá a partir de agora?',
+        "generos" : 'Ação  Drama  Legendado  Mistério  Seinen  Sobrenatural  Terror',
+        "dublagem" : 'Legendado',
+    },
+
+    {
+        "nome": "Jigoku Shoujo",
+        "lancamento": 2005,
+        "img": "./imgs/JigokuShoujo.png",
+        "episodios": 26,
+        "sinopse": "Uma garota misteriosa oferece às pessoas a chance de enviar seus inimigos para o inferno, mas com um preço: elas mesmas irão para o inferno quando morrerem. Uma série sobre vingança e as consequências de nossos atos.",
+        "generos": "Drama Fantasia Horror Mistério Sobrenatural",
+        "dublagem": "Legendado"
+    },
+    {
+        "nome": "Jojo's Bizarre Adventure Part 5",
+        "lancamento": 2016,
+        "img": "./imgs/JojosBizarreAdventurePart5.webp",
+        "episodios": 39,
+        "sinopse": "Na cidade costeira de Nápoles, a corrupção fervilha: a polícia conspira abertamente com bandidos, as drogas correm desenfreadas entre os jovens e a máfia governa as ruas com mão de ferro. No entanto, vários encontros fatídicos ocorrerão em breve. Entra Giorno Giovanna, um garoto de 15 anos com uma ligação excêntrica com a família Joestar, que ganha a vida com empregos de meio período e furtos de carteira. Além disso, ele é dotado da inexplicável habilidade Stand de dar e criar vida – cultivar plantas a partir do solo e transformar objetos inanimados em animais vivos, uma habilidade que ele apelidou de “Experiência Dourada”. Fascinado pelo poder dos gangsters locais, Giorno sonha em subir na hierarquia deles e se tornar uma “estrela da gangue”, um feito possível graças ao seu encontro com Bruno Bucciarati, um membro da gangue Passione com seu próprio senso de justiça. JoJo no Kimyou na Bouken: Ougon no Kaze segue os esforços de Giorno depois de se juntar à equipe de Bruno enquanto trabalhava para Passione, afastando outros gangsters e planejando secretamente derrubar seu misterioso chefe.",
+        "generos": "Ação Aventura Fantasia Shounen Sobrenatural",
+        "dublagem": "Legendado/Dublado"
+    },
+    {
+        "nome": "Sword Art Online",
+        "lancamento": 2012,
+        "img": "./imgs/SwordArtOnline.PNG",
+        "episodios": 25,
+        "sinopse": "Milhares de jogadores ficam presos em um RPG online e a única forma de sair é completar o jogo. Kirito, um jogador habilidoso, deve encontrar uma maneira de escapar e salvar seus amigos.",
+        "generos": "Ação Aventura Fantasia Sci-fi",
+        "dublagem": "Legendado/Dublado"
+    },
+    {
+        "nome": "Os Cavaleiros do Zodíaco: The Lost Canvas",
+        "lancamento": 2007,
+        "img": "./imgs/OsCavaleirosdoZodíacoTheLostCanvas.webp",
+        "episodios": 26,
+        "sinopse": "O conto sobre a guerra sagrada que ocorreu durante o século 18 é agora revelado. Tenma, Alone e Sasha, três órfãos que passaram pela infância se protegendo e depois que foram separados fizeram um pacto para se reencontrar. Eles se encontram novamente conectados, mas agora em papéis que nunca esperavam como possíveis. Pegasus, Hades e Atena. Agora é hora de lutar, mas o destino pode ser amargo, já que a guerra entre as forças de Atena e Hades pode colocar velhos amigos e famílias frente a frente.",
+        "generos": "Ação Aventura Fantasia Shounen",
+        "dublagem": "Legendado"
+    },
+    {
+        "nome": "Digimon Adventure",
+        "lancamento": 1999,
+        "img": "./imgs/DigimonAdventure.jpg",
+        "episodios": 54,
+        "sinopse": "Um grupo de crianças são transportadas para um mundo digital onde se tornam parceiros de Digimons. Juntos, eles devem lutar para voltar para casa e salvar ambos os mundos.",
+        "generos": "Ação Aventura Fantasia Shounen",
+        "dublagem": "Legendado/Dublado"
+    },
+
+];
